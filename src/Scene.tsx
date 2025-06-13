d7fcd4cbccb19a8506671b1ab709ffdce8e8a992
sarah <sarah.buisson@gmail.com>
2025-06-13 23:05:00 +0200

    working init

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -7,9 +7,37 @@ import { BoxGeometry, Mesh, MeshBasicMaterial } from 'three'
 import { Cube } from './components/Cube'
 import { Plane } from './components/Plane'
 import { Sphere } from './components/Sphere'
+import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
+import { Player } from './components/Player';
+import { useMouseCapture } from './components/useMouseCapture';
+import { useKeyboard } from './components/useKeyboard'; // Components for handling physics.
+
+
+function getInput(keyboard:any, mouse:{x:number,y:number}) {
+    let [x, y, z] = [0, 0, 0];
+    let running = false
+    // Checking keyboard inputs to determine movement direction
+    if (keyboard.get("s")) z += 1.0; // Move backward
+    if (keyboard.get("z")) z -= 1.0; // Move forward
+    if (keyboard.get("d")) x += 1.0; // Move right
+    if (keyboard.get("q")) x -= 1.0; // Move left
+    if (keyboard.get(" ")) y += 1.0; // Jump
+
+    // Returning an object with the movement and look direction
+    console.log(keyboard,keyboard["s"],x,y,z
+    )
+    return {
+        move: [x, y, z],
+        look: [mouse.x / window.innerWidth, mouse.y / window.innerHeight], // Mouse look direction
+        running: keyboard.get("Shift"), // Boolean to determine if the player is running (Shift key pressed)
+    };
+}
 
 function Scene() {
-  const { performance } = useControls('Monitoring', {
+    const keyboard = useKeyboard(); // Hook to get keyboard input
+    const mouse = useMouseCapture(); // Hook to get mouse input
+
+    const { performance } = useControls('Monitoring', {
     performance: false,
   })
 
@@ -19,6 +47,8 @@ function Scene() {
 
   const cubeRef = useRef<Mesh<BoxGeometry, MeshBasicMaterial>>(null)
 
+
+
   useFrame((_, delta) => {
     if (animate) {
       cubeRef.current!.rotation.y += delta / 3
@@ -26,10 +56,11 @@ function Scene() {
   })
 
   return (
+    //camera orbitale <OrbitControls makeDefault />-->
     <>
       {performance && <Perf position='top-left' />}
 
-      <OrbitControls makeDefault />
+
 
       <directionalLight
         position={[-2, 2, 3]}
@@ -38,10 +69,12 @@ function Scene() {
         shadow-mapSize={[1024 * 2, 1024 * 2]}
       />
       <ambientLight intensity={0.2} />
-
+        <Physics debug>
       <Cube ref={cubeRef} />
       <Sphere />
       <Plane />
+            <Player walk={2} jump={5} input={() => getInput(keyboard, mouse)} />
+        </Physics>
     </>
   )
 }

da88896777709db01c407bffd95920de1b0ffac2
sarah <sarah.buisson@gmail.com>
2025-06-15 01:16:55 +0200

    Good ground

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,19 +1,18 @@
-import { OrbitControls } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 import { useControls } from 'leva'
 import { Perf } from 'r3f-perf'
-import { useRef } from 'react'
+import React, { useRef } from 'react'
 import { BoxGeometry, Mesh, MeshBasicMaterial } from 'three'
 import { Cube } from './components/Cube'
-import { Plane } from './components/Plane'
 import { Sphere } from './components/Sphere'
-import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
+import { Physics } from "@react-three/rapier";
 import { Player } from './components/Player';
 import { useMouseCapture } from './components/useMouseCapture';
-import { useKeyboard } from './components/useKeyboard'; // Components for handling physics.
+import { useKeyboard } from './components/useKeyboard';
+import { Ground5 } from './components/Ground5'; // Components for handling physics.
 
 
-function getInput(keyboard:any, mouse:{x:number,y:number}) {
+function getInput(keyboard: any, mouse: { x: number, y: number }) {
     let [x, y, z] = [0, 0, 0];
     let running = false
     // Checking keyboard inputs to determine movement direction
@@ -24,7 +23,7 @@ function getInput(keyboard:any, mouse:{x:number,y:number}) {
     if (keyboard.get(" ")) y += 1.0; // Jump
 
     // Returning an object with the movement and look direction
-    console.log(keyboard,keyboard["s"],x,y,z
+    console.log(keyboard, keyboard["s"], x, y, z
     )
     return {
         move: [x, y, z],
@@ -37,46 +36,44 @@ function Scene() {
     const keyboard = useKeyboard(); // Hook to get keyboard input
     const mouse = useMouseCapture(); // Hook to get mouse input
 
-    const { performance } = useControls('Monitoring', {
-    performance: false,
-  })
+    const {performance} = useControls('Monitoring', {
+        performance: false,
+    })
 
-  const { animate } = useControls('Cube', {
-    animate: true,
-  })
+    const {animate} = useControls('Cube', {
+        animate: true,
+    })
 
-  const cubeRef = useRef<Mesh<BoxGeometry, MeshBasicMaterial>>(null)
-
-
-
-  useFrame((_, delta) => {
-    if (animate) {
-      cubeRef.current!.rotation.y += delta / 3
-    }
-  })
-
-  return (
-    //camera orbitale <OrbitControls makeDefault />-->
-    <>
-      {performance && <Perf position='top-left' />}
+    const cubeRef = useRef<Mesh<BoxGeometry, MeshBasicMaterial>>(null)
 
 
+    useFrame((_, delta) => {
+        if (animate) {
+            cubeRef.current!.rotation.y += delta / 3
+        }
+    })
 
-      <directionalLight
-        position={[-2, 2, 3]}
-        intensity={1.5}
-        castShadow
-        shadow-mapSize={[1024 * 2, 1024 * 2]}
-      />
-      <ambientLight intensity={0.2} />
-        <Physics debug>
-      <Cube ref={cubeRef} />
-      <Sphere />
-      <Plane />
-            <Player walk={2} jump={5} input={() => getInput(keyboard, mouse)} />
-        </Physics>
-    </>
-  )
+    return (
+        //camera orbitale <OrbitControls makeDefault />-->
+        <>
+            {performance && <Perf position='top-left'/>}
+
+
+            <directionalLight
+                position={[-2, 2, 3]}
+                intensity={1.5}
+                castShadow
+                shadow-mapSize={[1024 * 2, 1024 * 2]}
+            />
+            <ambientLight intensity={0.2}/>
+            <Physics debug>
+                <Cube ref={cubeRef}/>
+                <Sphere/>
+                <Ground5></Ground5>
+                <Player walk={2} jump={5} input={() => getInput(keyboard, mouse)}/>
+            </Physics>
+        </>
+    )
 }
 
 export { Scene }
---
+++ src/components/Ground5.tsx
@@ -0,0 +1,44 @@
+
+
+import { useTexture } from "@react-three/drei";
+import { CuboidCollider, HeightfieldCollider, RigidBody } from "@react-three/rapier";
+import { PlaneGeometry, Texture } from 'three';
+import React from 'react';
+
+const heightFieldHeight = 100;
+const heightFieldWidth = 100;
+const heightField = Array.from({
+    length: heightFieldHeight * heightFieldWidth
+}).map((_, index) => {
+    return Math.random()*2-1;
+});
+
+const heightFieldGeometry = new PlaneGeometry(
+    heightFieldWidth,
+    heightFieldHeight,
+    heightFieldWidth - 1,
+    heightFieldHeight - 1
+);
+
+heightField.forEach((v, index) => {
+    heightFieldGeometry.attributes.position.array[index * 3 + 2] = v;
+});
+heightFieldGeometry.scale(1, -1, 1);
+heightFieldGeometry.rotateX(-Math.PI / 2);
+heightFieldGeometry.rotateY(-Math.PI / 2);
+heightFieldGeometry.computeVertexNormals();
+
+export function Ground5(){
+    return  <RigidBody colliders={false} position={[0, -8, 0]}>
+    <mesh geometry={heightFieldGeometry} castShadow receiveShadow>
+        <meshPhysicalMaterial color="orange" side={2} />
+    </mesh>
+    <HeightfieldCollider
+        args={[
+            heightFieldWidth - 1,
+            heightFieldHeight - 1,
+            heightField,
+            { x: heightFieldWidth, y: 1, z: heightFieldHeight }
+        ]}
+    />
+</RigidBody>}
